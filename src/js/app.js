60aeededd8483ccbe48b478b85240ac2c78ede3f
jshuadvd <jshuadvd@icloud.com>
2016-11-30 23:12:55 -0800

    Rain added, but not animating

--- src/js/app.js
+++ src/js/app.js
@@ -321,17 +321,25 @@ audio.play();
 
 var camera = void 0,
     container = void 0,
+    color = void 0,
     controls = void 0,
     clock = void 0,
+    h = void 0,
     info = void 0,
     marker = void 0,
     mesh = void 0,
+    materials = [],
     mousePos = void 0,
+    parameters = void 0,
+    particles = void 0,
+    rainGeometry = void 0,
     renderer = void 0,
     raycaster = void 0,
     scene = void 0,
+    size = void 0,
     spotLight = void 0,
     spotLightHelper = void 0,
+    sprite = void 0,
     stats = void 0;
 
 var isUserInteracting = true,
@@ -382,30 +390,31 @@ function init() {
 	});
 
 	// instantiate a loader
-	var loader = new THREE.ImageLoader();
-
-	// load a image resource
-	loader.load(
-	// resource URL
-	'textures/assassins_creed_logo.png',
-	// Function when resource is loaded
-	function (image) {
-		// do something with it
-
-		// like drawing a part of it on a canvas
-		var canvas = document.createElement('canvas');
-		var context = canvas.getContext('2d');
-		context.drawImage(image, 100, 100);
-	},
-	// Function called when download progresses
-	function (xhr) {
-		console.log(xhr.loaded / xhr.total * 100 + '% loaded');
-	},
-	// Function called when download errors
-	function (xhr) {
-		console.log('An error happened');
-	});
-	scene.add(loader);
+	// var loader = new THREE.ImageLoader();
+	// 
+	// // load a image resource
+	// loader.load(
+	// 	// resource URL
+	// 	'textures/assassins_creed_logo.png',
+	// 	// Function when resource is loaded
+	// 	function ( image ) {
+	// 		// do something with it
+	// 
+	// 		// like drawing a part of it on a canvas
+	// 		var canvas = document.createElement( 'canvas' );
+	// 		var context = canvas.getContext( '2d' );
+	// 		context.drawImage( image, 100, 100 );
+	// 	},
+	// 	// Function called when download progresses
+	// 	function ( xhr ) {
+	// 		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	// 	},
+	// 	// Function called when download errors
+	// 	function ( xhr ) {
+	// 		console.log( 'An error happened' );
+	// 	}
+	// );
+	// scene.add(loader)
 
 	mesh = new THREE.Mesh(geometry, material);
 	scene.add(mesh);
@@ -533,7 +542,7 @@ function init() {
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	container.appendChild(renderer.domElement);
-	container.addEventListener("mousemove", getPosition, false);
+	container.addEventListener("mousedown", getPosition, false);
 
 	// document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 	document.addEventListener('mousemove', onDocumentMouseMove, false);
@@ -586,6 +595,44 @@ function init() {
 	initRain();
 }
 
+function initRain() {
+	rainGeometry = new THREE.Geometry();
+
+	var sprite1 = THREE.ImageUtils.loadTexture("textures/rain1.png"),
+	    sprite2 = THREE.ImageUtils.loadTexture("textures/rain2.png"),
+	    sprite3 = THREE.ImageUtils.loadTexture("textures/rain3.png"),
+	    sprite4 = THREE.ImageUtils.loadTexture("textures/rain4.png"),
+	    sprite5 = THREE.ImageUtils.loadTexture("textures/rain5.png");
+
+	for (var i = 0; i < rainDensity; i++) {
+
+		var vertex = new THREE.Vector3();
+		vertex.x = Math.random() * 2000 - 1000;
+		vertex.y = Math.random() * 4000 + 500;
+		vertex.z = Math.random() * 2000 - 1000;
+
+		rainGeometry.vertices.push(vertex);
+	}
+
+	parameters = [[[1.0, 0.2, 0.5], sprite2, 20], [[0.95, 0.1, 0.5], sprite3, 15], [[0.90, 0.05, 0.5], sprite1, 10], [[0.85, 0, 0.5], sprite5, 8], [[0.80, 0, 0.5], sprite4, 5]];
+
+	for (var i = 0; i < parameters.length; i++) {
+
+		color = parameters[i][0];
+		sprite = parameters[i][1];
+		size = parameters[i][2];
+
+		materials[i] = new THREE.PointCloudMaterial({ size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true });
+		materials[i].color.setHSL(color[0], color[1], color[2]);
+
+		particles = new THREE.PointCloud(rainGeometry, materials[i]);
+
+		particles.rotation.z = Math.random() * 0.20 + 0.10;
+
+		scene.add(particles);
+	}
+}
+
 function onWindowResize() {
 
 	camera.aspect = window.innerWidth / window.innerHeight;
@@ -635,6 +682,7 @@ function onDocumentMouseWheel(event) {
 }
 
 function animate() {
+	animateRain();
 
 	requestAnimationFrame(animate);
 	update();
@@ -665,11 +713,44 @@ function update() {
 	// spotLight.target = marker;
 	// controls.update()
 
-
 	spotLightHelper.update();
 	stats.update();
 	// rainEngine.update(0.01 * 0.5)
 	renderer.render(scene, camera);
+}
+
+function animateRain() {
+	var time = Date.now() * 0.00005;
+
+	for (var i = 0; i < scene.children.length; i++) {
+
+		var object = scene.children[i];
+
+		if (object instanceof THREE.PointCloud) {
+
+			if (i == 0) {
+				object.translateY(-10);
+			}
+
+			if (i > 0) {
+				if (layer) object.translateY(-10);else if (scene.children[i - 1].position.y < window.innerHeight * -1 / 2 - 1000) object.translateY(-10);
+			}
+
+			if (object.position.y < window.innerHeight * -1 * 5) {
+				object.position.y = 500;
+				object.position.x = 0;
+				if (i == 0) layer = true;
+			}
+		}
+	}
+
+	for (var i = 0; i < materials.length; i++) {
+
+		color = parameters[i][0];
+
+		h = 360 * (color[0] + time) % 360 / 360;
+		materials[i].color.setHSL(h, color[1], color[2]);
+	}
 }
 
 function getPosition(event) {
