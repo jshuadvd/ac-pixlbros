8f771b4d8f00386b7ad3103bb7d541922c491ed7
jshuadvd <jshuadvd@icloud.com>
2016-12-06 01:33:20 -0800

    Add audio back in

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -40,7 +40,7 @@ let audio = document.createElement('audio');
 let source = document.createElement('source');
 source.src = '/audio/AC-Trailer.mp3';
 audio.appendChild(source);
-// audio.play();
+audio.play();
 
 //************************************************************************//
 //                              Variables                           	  //
