dab0b73f27b7404b2f22abffe7154147638a0e01
Eric Walti <ewalti@gmail.com>
2016-12-08 13:20:42 -0800

    remove garbage

--- src/js/app.js
+++ src/js/app.js
@@ -331,38 +331,6 @@ function init() {
 	document.addEventListener('mouseup', onDocumentMouseUp, false);
 	document.addEventListener('touchstart', onDocumentTouchStart, false);
 	document.addEventListener('wheel', onDocumentMouseWheel, false);
-
-	document.addEventListener('dragover', function (event) {
-
-		event.preventDefault();
-		event.dataTransfer.dropEffect = 'copy';
-	}, false);
-
-	document.addEventListener('dragenter', function (event) {
-
-		document.body.style.opacity = 0.5;
-	}, false);
-
-	document.addEventListener('dragleave', function (event) {
-
-		document.body.style.opacity = 1;
-	}, false);
-
-	document.addEventListener('drop', function (event) {
-
-		event.preventDefault();
-
-		var reader = new FileReader();
-		reader.addEventListener('load', function (event) {
-
-			material.map.image.src = event.target.result;
-			material.map.needsUpdate = true;
-		}, false);
-		reader.readAsDataURL(event.dataTransfer.files[0]);
-
-		document.body.style.opacity = 1;
-	}, false);
-
 	window.addEventListener('resize', onWindowResize, false);
 
 	initRain();
