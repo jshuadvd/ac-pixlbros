c6381a33cd68279b3ebd0d04ca28da53d81035b3
jshuadvd <jshuadvd@icloud.com>
2017-04-05 17:41:01 -0700

    Updated functions list

--- src/js/app.js
+++ src/js/app.js
@@ -979,7 +979,6 @@ function update() {
 	rotateHotspots();
 
 	theta += 0.1;
-	// camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
 	// camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
 	// camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
 	// camera.lookAt( scene.position );
